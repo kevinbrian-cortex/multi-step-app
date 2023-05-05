00d53b2d42b3aced07d002248808d88f4c5c4b50
Kevin Neema Brian <kevinbrian@cortex-moz.com>
2023-05-05 06:56:53 +0200

    Update src/components/steps/addons.js

--- src/components/steps/addons.js
+++ src/components/steps/addons.js
@@ -1,12 +1,13 @@
 export default () => {
   return (
-    <div>
-      <div>
-        <h6>Pick addons</h6>
-        <p>Add-ons help enhance your gaming experience.</p>
+    <div className="flex flex-col gap-8">
+
+      <div className>
+        <h1 className="text-xl font-bold text-blue-900">Pick add-ons</h1>
+        <p className="text-gray-500">Add-ons help enhance your gaming experience.</p>
       </div>
 
-      <div>
+      <div className="flex flex-col gap-3">
         {[1, 2, 3].map((item) => (
           <Addons />
         ))}
@@ -17,19 +18,21 @@ export default () => {
 
 const Addons = () => {
   return (
-    <div>
-      <div>
+    <div className="flex flex-row items-center gap-4 border p-2 rounded hover:bg-slate-100 hover:border-blue-950 hover:cursor-pointer">
+
+      <div className="p-2">
         <input type="checkbox" />
       </div>
 
-      <div>
-        <div>
-          <strong>Online service</strong>
-          <p>Access to multiplayer games</p>
+      <div className="flex-1 flex justify-between items-center">
+
+        <div className="flex flex-col">
+          <span className="text-blue-900 font-medium">Online service</span>
+          <span className="text-sm text-gray-500 font-sm">Access to multiplayer games</span>
         </div>
 
-        <div>
-          <strong>+$1/mo</strong>
+        <div className="p-2">
+          <span className="font-medium text-sm text-blue-950">+$1/mo</span>
         </div>
       </div>
     </div>
